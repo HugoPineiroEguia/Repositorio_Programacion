e5746dfd7d19045e6d4a79520985976b21cee38b
1 <mperezperez@danielcastelao.org>
2022-06-05 16:46:37 +0200

    Clase Personaje: plantilla para jugador y monstruo

---
+++ src/Juego/Personaje.java
@@ -0,0 +1,23 @@
+package Juego;
+
+public class Personaje {
+    int hp;
+    int ad;
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getAd() {
+        return ad;
+    }
+
+    public void setAd(int ad) {
+        this.ad = ad;
+    }
+
+}
